b01cccbf62b8f592a1cd58159d08bb8c85321c56
Anthony Gascón <agascon@stratio.com>
2020-12-11 11:19:29 -0500

    released version 1.0.1

--- release.sh
+++ release.sh
@@ -3,9 +3,11 @@
 
 
 mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
-git clone https://github.com/anthony-gascon/mvnrepo.git
+
+git clone https://github.com/anthony-gascon/mvnrepo.git /tmp
+mvn deploy -Durl=file:///tmp/mvnrepo -DcreateChecksum=true
+
 cd /tmp/mvnrepo
-mvn install -DlocalRepositoryPath=. -DcreateChecksum=true
 git add -A . && git commit -m "released version $1"
 git push
 
