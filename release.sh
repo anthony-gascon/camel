edd732519b9bc14f7c31d48864f04857a27ec2fb
Anthony Gascón <agascon@stratio.com>
2020-12-11 12:18:25 -0500

    add release script

--- release.sh
+++ release.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-rm -rf /tmp/mvnrepo
+#rm -rf /tmp/mvnrepo
 
-mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
+#mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
 
-git clone https://github.com/anthony-gascon/mvnrepo.git /tmp
-mvn deploy -DaltDeploymentRepository=local::file:tmp/mvnrepo -DcreateChecksum=true
+#git clone https://github.com/anthony-gascon/mvnrepo.git /tmp/mvnrepo
+
+mvn deploy -DaltDeploymentRepository=local::default::file:/tmp/mvnrepo -DcreateChecksum=true
 
 cd /tmp/mvnrepo
 git add -A . && git commit -m "released version $1"
